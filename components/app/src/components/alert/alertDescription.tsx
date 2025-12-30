b8acf5a8ec79f764cdcac623cc175aec3cc67afb
Shohaum <shohaumsarkar03@gmail.com>
2025-12-30 12:51:18 +0530

    fix alert styles

--- components/app/src/components/alert/alertDescription.tsx
+++ components/app/src/components/alert/alertDescription.tsx
@@ -1,12 +1,16 @@
+// utilities
+import React from "react";
 // CSS
 import styles from "@/components/alert/alertDescription.module.css";
 // types
 import { AlertDescriptionProps } from "@/types/alert/alert";
 
-const AlertDescription = ({ children }: AlertDescriptionProps) => {
-    return <div className={styles.alertDescription}>
+const AlertDescription = React.forwardRef<HTMLDivElement, AlertDescriptionProps>(({ children, ...props }, ref) => {
+    return <div {...props} ref={ref} className={styles.alertDescription}>
         {children}
     </div>;
-};
+});
+
+AlertDescription.displayName = "AlertDescription";
 
 export default AlertDescription;
--- components/app/src/components/alert/alertTitle.tsx
+++ components/app/src/components/alert/alertTitle.tsx
@@ -1,12 +1,16 @@
+// utilities
+import React from "react";
 // CSS
 import styles from "@/components/alert/alertTitle.module.css";
 // types
 import { AlertTitleProps } from "@/types/alert/alert";
 
-const AlertTitle = ({ children }: AlertTitleProps) => {
-    return <h4 className={styles.alertTitle}>
+const AlertTitle = React.forwardRef<HTMLDivElement, AlertTitleProps>(({ children, ...props }, ref) => {
+    return <h4 {...props} ref={ref} className={styles.alertTitle}>
         {children}
     </h4>;
-};
+});
+
+AlertTitle.displayName = "AlertTitle";
 
 export default AlertTitle;
