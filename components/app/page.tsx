c174486892ab8dee2185173341db52ea7642d1dc
Shohaum <shohaumsarkar03@gmail.com>
2026-01-03 13:09:26 +0530

    fix some styling of avatar component

--- components/app/page.tsx
+++ components/app/page.tsx
@@ -472,7 +472,7 @@ const App = () => {
                 </div>
  */}
                 {/* aspect ratio */}
-                <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
+                {/* <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
                     <AspectRatio style={{ width: "min(16rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={9 / 16}>
                         <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
                             alt="Photo by Drew Beamer" fill />
@@ -497,46 +497,50 @@ const App = () => {
                         <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
                             alt="Photo by Drew Beamer" fill />
                     </AspectRatio>
-                </div>
+                </div> */}
 
                 {/* Avatar */}
-                {/* <div style={{ width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem', padding: '1rem' }}>
+                <div style={{ width: "fit-content", height: "fit-content", display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem' }}>
 
-                    <Avatar width={40}>
-                        <AvatarImage grayScale={true} src="https://github.com/deno.png" alt="@deno" />
-                        <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>DN</AvatarFallback>
-                    </Avatar>
+                    <div style={{ display: 'flex', gap: '0.5rem' }}>
+                        <Avatar width={36}>
+                            <AvatarImage grayScale={true} src="https://github.com/deno.png" alt="@deno" />
+                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>DN</AvatarFallback>
+                        </Avatar>
 
-                    <Avatar showTooltip={true} name="Vercel" designation="SDE - Vercel" width={50}>
-                        <AvatarImage src="https://github.com/vercel.png" alt="@vercel" />
-                        <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>MB</AvatarFallback>
-                    </Avatar>
+                        <Avatar showTooltip={true} name="Vercel" designation="SDE - Vercel" width={36}>
+                            <AvatarImage src="https://github.com/vercel.png" alt="@vercel" />
+                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>MB</AvatarFallback>
+                        </Avatar>
+                    </div>
 
-                    <AvatarCollapsed>
+                    <div>
+                        <AvatarCollapsed>
 
-                        <Avatar showTooltip={true} name="Shohaum" designation="Founder - Shohaum" width={40}>
-                            <AvatarImage src="https://github.com/shohaum.png" alt="@shohaum" />
-                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>SS</AvatarFallback>
-                        </Avatar>
-                        <Avatar showTooltip={true} name="Redis" designation="Redis Developer" width={40}>
-                            <AvatarImage src="https://github.com/redis.png" alt="@redis" />
-                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>RR</AvatarFallback>
-                        </Avatar>
-                        <Avatar showTooltip={true} name="Excalidraw" designation="Product Lead - Excalidraw" width={40}>
-                            <AvatarImage src="https://github.com/excalidraw.png" alt="@excalidraw" />
-                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>ER</AvatarFallback>
-                        </Avatar>
-                        <Avatar showTooltip={true} name="Evil Rabbit" designation="Product Designer" width={40}>
-                            <AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
-                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>ER</AvatarFallback>
-                        </Avatar>
-                        <Avatar showTooltip={true} name="OpenAI" designation="Founder - OpenAI" width={40}>
-                            <AvatarImage src="https://github.com/openai.png" alt="@openai" />
-                            <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>OA</AvatarFallback>
-                        </Avatar>
-                        
-                    </AvatarCollapsed>
-                </div> */}
+                            <Avatar showTooltip={true} name="Shohaum" designation="Founder - Shohaum" width={36}>
+                                <AvatarImage src="https://github.com/shohaum.png" alt="@shohaum" />
+                                <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>SS</AvatarFallback>
+                            </Avatar>
+                            <Avatar showTooltip={true} name="Redis" designation="Redis Developer" width={36}>
+                                <AvatarImage src="https://github.com/redis.png" alt="@redis" />
+                                <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>RR</AvatarFallback>
+                            </Avatar>
+                            <Avatar showTooltip={true} name="Excalidraw" designation="Product Lead - Excalidraw" width={36}>
+                                <AvatarImage src="https://github.com/excalidraw.png" alt="@excalidraw" />
+                                <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>ER</AvatarFallback>
+                            </Avatar>
+                            <Avatar showTooltip={true} name="Evil Rabbit" designation="Product Designer" width={36}>
+                                <AvatarImage src="https://github.com/evilrabbit.png" alt="@evilrabbit" />
+                                <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>ER</AvatarFallback>
+                            </Avatar>
+                            <Avatar showTooltip={true} name="OpenAI" designation="Founder - OpenAI" width={36}>
+                                <AvatarImage src="https://github.com/openai.png" alt="@openai" />
+                                <AvatarFallback style={{ backgroundColor: "var(--gray-200)", color: "var(--gray-700)", fontSize: "var(--size-sm)", fontWeight: 500 }}>OA</AvatarFallback>
+                            </Avatar>
+
+                        </AvatarCollapsed>
+                    </div>
+                </div>
 
                 {/* breadcrumb */}
                 {/* <Breadcrumb>
