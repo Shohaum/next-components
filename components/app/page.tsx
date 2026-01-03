fa5b9ef6e8226373e8d006cf671b3c1cb525e622
Shohaum <shohaumsarkar03@gmail.com>
2026-01-03 22:05:58 +0530

    add page.tsx

--- components/app/page.tsx
+++ components/app/page.tsx
@@ -41,8 +41,6 @@ import AccordionItem from "@/components/accordion/accordionItem";
 import AccordionItemTrigger from "@/components/accordion/accordionItemTrigger";
 import AccordionItemContent from "@/components/accordion/accordionItemContent";
 
-import AspectRatio from "@/components/aspectRatio/aspectRatio";
-
 import { data } from "@/data/data";
 import Image from "next/image";
 
@@ -326,34 +324,6 @@ const App = () => {
                         </TableBody>
                         <Caption>A list of your recent invoices</Caption>
                     </Table>
-                </div> */}
-
-                {/* aspect ratio */}
-                {/* <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
-                    <AspectRatio style={{ width: "min(16rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={9 / 16}>
-                        <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                            alt="Photo by Drew Beamer" fill />
-                    </AspectRatio>
-                    <AspectRatio style={{ width: "min(23rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={16 / 9}>
-                        <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                            alt="Photo by Drew Beamer" fill />
-                    </AspectRatio>
-                    <AspectRatio style={{ width: "min(16rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={4 / 3}>
-                        <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                            alt="Photo by Drew Beamer" fill />
-                    </AspectRatio>
-                    <AspectRatio style={{ width: "min(16rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={3 / 2}>
-                        <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                            alt="Photo by Drew Beamer" fill />
-                    </AspectRatio>
-                    <AspectRatio style={{ width: "min(10rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={1}>
-                        <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                            alt="Photo by Drew Beamer" fill />
-                    </AspectRatio>
-                    <AspectRatio style={{ width: "min(16rem, 90%)", outline: "1px solid var(--border-color)", borderRadius: "0.5rem" }} ratio={21 / 9}>
-                        <Image src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                            alt="Photo by Drew Beamer" fill />
-                    </AspectRatio>
                 </div> */}
 
                 {/* Avatar */}
