ad374339ff5f1973f492ae6462ff2443cc58abad
Shohaum <shohaumsarkar03@gmail.com>
2026-01-04 23:42:47 +0530

    change the main page.tsx

--- components/app/src/components/table/caption.tsx
+++ components/app/src/components/table/caption.tsx
@@ -1,13 +1,17 @@
 // CSS
 import styles from "@/components/table/caption.module.css";
+// utilities
+import React from "react";
 // types
 import { CaptionProps } from "@/types/table/table";
-const Caption = ({ children }: CaptionProps) => {
+const Caption = React.forwardRef<HTMLTableCaptionElement, CaptionProps>(({ children, ...props }, ref) => {
     return (
-        <caption aria-label="tableCaption" data-slot="tableCaption" className={styles.caption}>
+        <caption {...props} ref={ref} data-slot="tableCaption" className={`${styles.caption} ${props.className || ""}`}>
             {children}
         </caption>
     );
-}
+});
+
+Caption.displayName = "TableCaption";
 
 export default Caption;
--- components/app/src/components/table/tableHeadCell.tsx
+++ components/app/src/components/table/tableHeadCell.tsx
@@ -1,13 +1,17 @@
 // CSS
 import styles from "@/components/table/tableHeadCell.module.css";
+// utilities
+import React from "react";
 // types
 import { TableHeadCellProps } from "@/types/table/table";
-const TableHeadCell = ({ children }: TableHeadCellProps) => {
+const TableHeadCell = React.forwardRef<HTMLTableCellElement, TableHeadCellProps>(({ children, ...props }, ref) => {
     return (
-        <th className={styles.tableHeadCell}>
+        <th {...props} ref={ref} className={`${styles.tableHeadCell} ${props.className || ""}`}>
             {children}
         </th>
     );
-}
+});
+
+TableHeadCell.displayName = "TableHeadCell";
 
 export default TableHeadCell;
